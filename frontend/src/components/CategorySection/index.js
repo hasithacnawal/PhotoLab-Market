ff1310064f76ba1ca45ac0cefe6ce9dc07564c8e
HDsandaruwan <53431808+HDanushka@users.noreply.github.com>
2021-02-21 15:31:57 +0530

    CategorySection is added.

---
+++ frontend/src/components/CategorySection/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import Icon1 from '../../images/flowers.jpg';
+import Icon2 from '../../images/love.jpg';
+import Icon3 from '../../images/wallpaper.jpg';
+
+
+const  Category = () => {
+    return (
+        <CategoryContainer id="Category" >
+            <CategoryH1>Categories</CategoryH1>
+            <CategoryWrapper>
+                
+                <CategoryCard>
+                    <CategoryIcon src={Icon1} />
+                    <CategoryH2>Flowers</CategoryH2>
+                </CategoryCard>
+
+                <CategoryCard>
+                    <CategoryIcon src={Icon2} />
+                    <CategoryH2>Love</CategoryH2>
+                </CategoryCard>
+
+                <CategoryCard>
+                    <CategoryIcon src={Icon3} />
+                    <CategoryH2>Wallpapers</CategoryH2>
+                </CategoryCard>
+
+            </CategoryWrapper>
+        </CategoryContainer>
+    );
+};
+
+export default  Category;
