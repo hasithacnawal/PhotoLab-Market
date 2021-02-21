33867d5ced9205245a1afc8c6d15ef56eabd893a
JeewanthiLakmini <74044169+JeewanthiLakmini@users.noreply.github.com>
2021-02-21 16:30:37 +0530

    Footer and category section are added in to home page

--- frontend/src/pages/Homepage.js
+++ frontend/src/pages/Homepage.js
@@ -1,9 +1,10 @@
 import React from "react";
 import HomeTopSection from "../components/HomeTopSection/index";
-
+import Category from "../components/CategorySection/index";
+import Footer from "../components/Footer/index";
 
 function Homepage() {
-  return <div><HomeTopSection/></div>;
+  return <div><HomeTopSection/><Category/><Footer/></div>;
 }
 
 export default Homepage;
